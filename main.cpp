eff7ba3e7217766ab62a1eddebc626620d787b60
v1c7or20 <victorpa793@gmail.com>
2019-09-15 15:56:14 -0500

    done

--- main.cpp
+++ main.cpp
@@ -26,13 +26,22 @@ void change(char &orientacion, char w, char x, char y, char z){
     }
 }
 
+bool iscent(int *array[],int perdido, int x, int y ){
+    for (int i = 0; i <perdido ; i++) {
+        if(array[0][i] == x and array [1][i] == y){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
-    int x0,y0,x1=0,y1=0;
+    int x0,y0,x1=0,y1=0,perdidos = 0;
     char orientacion;
     string direcciones;
     bool islostx ,islosty ;
     cin>>x0>>y0;
-    int scent[2][x0*y0];
+   vector<vector<bool>> scent(y0 + 1, vector<bool> (x0 + 1, false));
 
     while(cin>>x1>>y1>>orientacion>>direcciones){
         islostx = false, islosty = false;
@@ -42,30 +51,59 @@ int main(){
                     case 'F':
                         switch (orientacion){
                             case 'N':
-                                y1=y1+1;
-                                islosty = lost(y0,y1);
+                                if(!scent[y1][x1]){
+                                    y1=y1+1;
+                                    islosty = lost(y0,y1);
+                                }else{
+                                    if ( y1+1<=y0){
+                                        y1=y1+1;
+                                        islosty = lost(y0,y1);
+                                    }
+                                }
                                 break;
                             case 'S':
-                                if(lost(y0,y1-1)){
-                                    islosty = lost(y0,y1-1);
-                                    y1=1;
+                                if(!scent[y1][x1]){
+                                    if(lost(y0,y1-1)){
+                                        islosty = lost(y0,y1-1);
+                                        y1=1;
+                                    }else{
+                                        y1=y1-1;
+                                        islosty = lost(y0,y1);
+                                    }
                                 }else{
-                                    y1=y1-1;
-                                    islosty = lost(y0,y1);
+                                    if ( y1-1>=0){
+                                        y1=y1-1;
+                                        islosty = lost(y0,y1);
+                                    }
                                 }
                                 break;
                             case 'E':
-                                x1=x1+1;
-                                islostx = lost(x0,x1);
+                                if (!scent[y1][x1]){
+                                    x1=x1+1;
+                                    islostx = lost(x0,x1);
+                                }else{
+                                    if ( x1+1<=x0){
+                                        x1=x1+1;
+                                        islosty = lost(x0,x1);
+                                    }
+                                }
                                 break;
                             case 'W':
-                                if(lost(x0,x1-1)){
-                                    islostx = lost(x0,x1-1);
-                                    x1=1;
+                                if (!scent[y1][x1]){
+                                    if(lost(x0,x1-1)){
+                                        islostx = lost(x0,x1-1);
+                                        x1=1;
+                                    }else{
+                                        x1=x1-1;
+                                        islostx = lost(x0,x1);
+                                    }
                                 }else{
-                                    x1=x1-1;
-                                    islostx = lost(x0,x1);
+                                    if ( x1-1>=0){
+                                        x1=x1-1;
+                                        islosty = lost(x0,x1);
+                                    }
                                 }
+                                break;
                             default:
                                 break;
                         }
@@ -83,9 +121,11 @@ int main(){
         }
         if (islostx or islosty){
             if (islostx){
+                scent[y1][x1-1] = true;
                 cout<<x1-1<<" "<<y1<<" "<<orientacion<<" LOST"<<endl;
             }
             else{
+                scent[y1-1][x1] = true;
                 cout<<x1<<" "<<y1-1<<" "<<orientacion<<" LOST"<<endl;
             }
         } else{
